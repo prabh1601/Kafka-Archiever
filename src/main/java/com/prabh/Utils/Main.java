b9d46fdc2e7a611ff52be1cf2ff0abbf97174325
prabh <prabh1601@gmail.com>
2022-05-29 20:08:49 +0530

    added basic use of consumer client

--- src/main/java/com/prabh/Utils/Main.java
+++ src/main/java/com/prabh/Utils/Main.java
@@ -1,9 +1,24 @@
 package com.prabh.Utils;
 
-import com.prabh.SinkArchiever.ConsumerThread;
+import com.prabh.SinkArchiever.ConsumerClient;
 
+// Template UseCase of Consumer Client
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+            ConsumerClient c = new ConsumerClient.Builder()
+                    .bootstrapServer("localhost:9092")
+                    .consumerGroup("cg")
+                    .consumerCount(4)
+                    .taskCount(8)
+                    .subscribedTopic("test")
+                    .build();
+
+            c.startConsumers();
+
+            Thread.sleep(5 * 1000);
+
+            c.stopConsumers();
+
 
     }
 }
