20a8118345f65941f04ad7fc52c098b651781479
prabh <prabh1601@gmail.com>
2022-06-03 00:41:19 +0530

    fix collector logic

--- src/main/java/com/prabh/SinkArchiever/Collector.java
+++ src/main/java/com/prabh/SinkArchiever/Collector.java
@@ -14,26 +14,14 @@ import com.prabh.Utils.Task;
 
 public class Collector {
     private final Logger logger = LoggerFactory.getLogger(Collector.class.getName());
-    private final ExecutorService taskExecutor;
-    private CountDownLatch canWrite;
-    private final ScheduledExecutorService batchUploader;
-
+    private ExecutorService taskExecutor;
 
     public Collector(int taskPoolSize) {
-        taskExecutor = Executors.newFixedThreadPool(taskPoolSize);
-        batchUploader = Executors.newScheduledThreadPool(1);
-        batchUploader.scheduleAtFixedRate(new Batch(), 10, 30, TimeUnit.SECONDS);
-        pauseWriting();
-        resumeWriting();
+        this.taskExecutor = Executors.newFixedThreadPool(taskPoolSize);
     }
 
     public Task submit(List<ConsumerRecord<String, String>> records) {
         Task t = new Task(records);
-        try {
-            canWrite.await();
-        } catch (InterruptedException e) {
-            logger.error("Batcher Await Interrupted");
-        }
         taskExecutor.submit(t);
         return t;
     }
@@ -41,43 +29,11 @@ public class Collector {
     public void stop() {
         logger.warn("Task Executor Shutting down");
         taskExecutor.shutdown();
-    }
-
-    public void pauseWriting() {
-        logger.info("Pausing the Writing Tasks");
-        canWrite = new CountDownLatch(1);
-    }
-
-    public void resumeWriting() {
-        logger.info("Resuming the Writing Tasks");
-        canWrite.countDown();
-    }
-
-    private class Batch implements Runnable {
-        AwsClient s3Client = new AwsClient();
-
-        public Batch() {
-        }
-
-        public void run() {
-            logger.info("Periodic Batch Upload scheduled");
-            // 1. stop further tasks to be started
-            pauseWriting();
-            // 2. wait till on ongoing writing tasks are finished (Another option : Maybe just kill them ?)
-
-            // 3. Change the name of current folder
-            File dir = new File("/mnt/Drive1/Write");
-            File newDir = new File(dir.getParent() + "/Upload");
-            dir.renameTo(newDir);
-            // 4. Make new folder of previous name
-            dir.mkdirs();
-            // 5. resume task loading
-            resumeWriting();
-            logger.info("Starting Batch uploading");
-            // 6. Upload folder
-            s3Client.uploadBatch();
-            // 7. Delete upload folder
-            newDir.delete();
+        try {
+            taskExecutor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
+        } catch (InterruptedException e) {
+            logger.error(e.getMessage(), e);
         }
     }
+
 }
