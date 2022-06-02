ec03ef5221f4801f9fbd190da58ef78ee022a524
prabh <prabh1601@gmail.com>
2022-06-02 20:36:21 +0530

    implement batcher service

--- src/main/java/com/prabh/SinkArchiever/Collector.java
+++ src/main/java/com/prabh/SinkArchiever/Collector.java
@@ -1,25 +1,39 @@
 package com.prabh.SinkArchiever;
 
+import com.amazonaws.services.customerprofiles.model.Batch;
+import com.prabh.Utils.AwsClient;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 import com.prabh.Utils.Task;
 
 public class Collector {
     private final Logger logger = LoggerFactory.getLogger(Collector.class.getName());
     private final ExecutorService taskExecutor;
+    private CountDownLatch canWrite;
+    private final ScheduledExecutorService batchUploader;
 
-    Collector(int size) {
-        taskExecutor = Executors.newFixedThreadPool(size);
+
+    public Collector(int taskPoolSize) {
+        taskExecutor = Executors.newFixedThreadPool(taskPoolSize);
+        batchUploader = Executors.newScheduledThreadPool(1);
+        batchUploader.scheduleAtFixedRate(new Batch(), 10, 30, TimeUnit.SECONDS);
+        pauseWriting();
+        resumeWriting();
     }
 
     public Task submit(List<ConsumerRecord<String, String>> records) {
         Task t = new Task(records);
+        try {
+            canWrite.await();
+        } catch (InterruptedException e) {
+            logger.error("Batcher Await Interrupted");
+        }
         taskExecutor.submit(t);
         return t;
     }
@@ -27,5 +41,43 @@ public class Collector {
     public void stop() {
         logger.warn("Task Executor Shutting down");
         taskExecutor.shutdown();
+    }
+
+    public void pauseWriting() {
+        logger.info("Pausing the Writing Tasks");
+        canWrite = new CountDownLatch(1);
+    }
+
+    public void resumeWriting() {
+        logger.info("Resuming the Writing Tasks");
+        canWrite.countDown();
+    }
+
+    private class Batch implements Runnable {
+        AwsClient s3Client = new AwsClient();
+
+        public Batch() {
+        }
+
+        public void run() {
+            logger.info("Periodic Batch Upload scheduled");
+            // 1. stop further tasks to be started
+            pauseWriting();
+            // 2. wait till on ongoing writing tasks are finished (Another option : Maybe just kill them ?)
+
+            // 3. Change the name of current folder
+            File dir = new File("/mnt/Drive1/Write");
+            File newDir = new File(dir.getParent() + "/Upload");
+            dir.renameTo(newDir);
+            // 4. Make new folder of previous name
+            dir.mkdirs();
+            // 5. resume task loading
+            resumeWriting();
+            logger.info("Starting Batch uploading");
+            // 6. Upload folder
+            s3Client.uploadBatch();
+            // 7. Delete upload folder
+            newDir.delete();
+        }
     }
 }
--- src/main/java/com/prabh/Utils/Task.java
+++ src/main/java/com/prabh/Utils/Task.java
@@ -1,14 +1,28 @@
 package com.prabh.Utils;
 
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.regions.Regions;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.google.gson.Gson;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.*;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.concurrent.locks.ReentrantLock;
+
 
 public class Task implements Runnable {
     private final Logger logger = LoggerFactory.getLogger(Task.class.getName());
@@ -32,18 +46,30 @@ public class Task implements Runnable {
         startStopLock.unlock();
 
         log();
-        for (ConsumerRecord<String, String> record : records) {
-            if (stopped) break;
-            // process records here
-            currentOffset.set(record.offset() + 1);
-        }
+        try {
+            final String writePath = "/mnt/Drive1/Write/";
+            final int partition = records.get(0).partition();
+            final long startingOffset = records.get(0).offset();
+            final long endingOffset = records.get(records.size() - 1).offset();
+            String fileName = writePath + partition + "-(" + startingOffset + "-" + endingOffset + ").txt";
+            BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
+            for (ConsumerRecord<String, String> record : records) {
+                if (stopped) break;
+                // Process this record
+                writer.write(record.value() + "\n");
+                currentOffset.set(record.offset() + 1);
+            }
 
+            writer.close();
+        } catch (IOException e) {
+            logger.error("Writing files abrupted");
+        }
         finished = true;
         completion.complete(currentOffset.get());
     }
 
     private void log() {
-        logger.info(Thread.currentThread().getName() + " got " + records.size() + " records from partition: " + records.get(0).partition());
+        logger.info(Thread.currentThread().getName() + " got " + records.size() + " records from partition " + records.get(0).partition());
     }
 
     public long getCurrentOffset() {
