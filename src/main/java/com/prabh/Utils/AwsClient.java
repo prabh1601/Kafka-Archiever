0b506e80032aec6845f45a02b3a1974fde6ca895
prabh <prabh1601@gmail.com>
2022-06-05 23:05:26 +0530

    minor updates

--- src/main/java/com/prabh/Utils/AwsClient.java
+++ src/main/java/com/prabh/Utils/AwsClient.java
@@ -9,8 +9,6 @@ import software.amazon.awssdk.transfer.s3.*;
 
 import java.io.*;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import static software.amazon.awssdk.transfer.s3.SizeConstant.MB;
@@ -18,6 +16,7 @@ import static software.amazon.awssdk.transfer.s3.SizeConstant.MB;
 public class AwsClient {
     private final Logger logger = LoggerFactory.getLogger(AwsClient.class.getName());
     Properties values = new Properties();
+    private S3TransferManager s3tm;
 
     public AwsClient() {
         try {
@@ -25,36 +24,30 @@ public class AwsClient {
         } catch (IOException e) {
             logger.error(e.getMessage(), e);
         }
+        initConnection();
     }
 
-    public void uploadBatch(String dirPath) {
-        Region region = Region.US_EAST_2;
+    public void initConnection() {
+        Region region = Region.AP_SOUTH_1;
         AwsBasicCredentials awsCreds = AwsBasicCredentials.create(values.getProperty("awsKeyId"), values.getProperty("awsSecretKey"));
         S3ClientConfiguration s3ClientConfiguration = S3ClientConfiguration.builder()
                 .region(region)
                 .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                .minimumPartSizeInBytes((long) (10 * MB))
+                .minimumPartSizeInBytes((long) (9 * MB))
                 .targetThroughputInGbps(20.0)
                 .build();
 
-        S3TransferManager s3tm = S3TransferManager.builder()
+        s3tm = S3TransferManager.builder()
                 .s3ClientConfiguration(s3ClientConfiguration)
                 .build();
-        File dir = new File(dirPath);
-        File[] files = dir.listFiles();
-        List<Upload> scheduledUploads = new ArrayList<>();
-        for (File file : files) {
-            Upload upload = s3tm.upload(b -> b.source(Paths.get(file.getAbsolutePath()))
-                    .putObjectRequest(req -> req.bucket(values.getProperty("bucketName"))
-                            .key(file.getName())));
-            scheduledUploads.add(upload);
+    }
 
-        }
+    public void upload(File file, String key) {
+        Upload upload = s3tm.upload(b -> b.source(Paths.get(file.getAbsolutePath()))
+                .putObjectRequest(req -> req.bucket(values.getProperty("bucketName"))
+                        .key(key)));
 
-        for (Upload upload : scheduledUploads) {
-            upload.completionFuture().join();
-            logger.info("Successfully uploaded file");
-        }
-        logger.info("Batch Upload Complete");
+        upload.completionFuture().join();
+        logger.info("Successfully uploaded file : {}", file.getName());
     }
 }
--- src/main/java/com/prabh/Utils/Main.java
+++ src/main/java/com/prabh/Utils/Main.java
@@ -1,11 +1,6 @@
 package com.prabh.Utils;
 
-import com.prabh.SinkArchiever.Collector;
 import com.prabh.SinkArchiever.ConsumerClient;
-
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
 
 // Template UseCase of Consumer Client
 public class Main {
@@ -14,12 +9,12 @@ public class Main {
                 .bootstrapServer("localhost:9092")
                 .consumerGroup("cg")
                 .consumerCount(3)
-                .taskCount(3)
+                .taskCount(12)
                 .subscribedTopic("test")
                 .build();
 
         c.start();
-        ScheduledExecutorService batchExecutor = Executors.newScheduledThreadPool(1);
-        batchExecutor.scheduleWithFixedDelay(new Batcher(c), 15, 30, TimeUnit.SECONDS);
+
+
     }
 }
