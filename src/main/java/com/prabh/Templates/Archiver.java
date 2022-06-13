b5dfa78c2ce425e2513414074efe8b01a13aa2bd
prabh <prabh1601@gmail.com>
2022-06-13 16:19:07 +0530

    add use templates

--- src/main/java/com/prabh/Templates/Archiver.java
+++ src/main/java/com/prabh/Templates/Archiver.java
@@ -1,11 +1,12 @@
-package com.prabh.Utils;
+package com.prabh.Templates;
 
 import com.prabh.SinkConnector.SinkApplication;
-import com.prabh.SourceConnector.DownloadingService;
-import com.prabh.SourceConnector.RequestObject;
+import com.prabh.Utils.AdminController;
+import com.prabh.Utils.Config;
+import org.apache.kafka.clients.admin.NewTopic;
 
 // Template UseCases
-public class Main {
+public class Archiver {
     public void SinkConnectorTemplate() {
         // SinkConnector
         SinkApplication app = new SinkApplication.Builder()
@@ -25,23 +26,7 @@ public class Main {
         }));
     }
 
-    public void SourceConnectorTemplate() {
-        // Source Connector
-        // Constructor : (year, month, date, hour, min) -> Not required to put all 5, any prefix of this values work
-        RequestObject start = new RequestObject
-                .StartTimestampBuilder(2022, 6, 10, 12, 5)
-                .build();
-
-        RequestObject end = new RequestObject
-                .EndTimestampBuilder(2022, 6, 10)
-                .build();
-
-        DownloadingService d = new DownloadingService("test", start, end);
-        d.run();
-    }
-
     public static void main(String[] args) throws InterruptedException {
-//        new Main().SinkConnectorTemplate();
-//        new Main().SourceConnectorTemplate();
+        new Archiver().SinkConnectorTemplate();
     }
 }
